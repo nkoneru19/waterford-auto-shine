3f35475eabfb2950f67f45b20feb768d0a61207a
Nikhil Koneru <nkoneru19@gmail.com>
2026-05-26 17:54:48 -0400

    Place logo icon to left of Waterford Auto Shine text in nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -66,15 +66,24 @@ export function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 lg:h-20">
           {/* Logo */}
-          <Link href="/" className="flex items-center shrink-0">
+          <Link href="/" className="flex items-center gap-2 shrink-0">
             <Image
               src="/images/logo.png"
               alt={siteConfig.name}
-              width={140}
-              height={56}
-              className="h-12 w-auto object-contain"
+              width={44}
+              height={44}
+              className="h-10 w-auto object-contain"
               priority
             />
+            <span
+              className="font-bold text-lg"
+              style={{
+                fontFamily: "var(--font-heading)",
+                color: "var(--color-accent)",
+              }}
+            >
+              {siteConfig.name}
+            </span>
           </Link>
 
           {/* Desktop Nav */}
